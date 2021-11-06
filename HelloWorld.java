0738a876c4ee80f697ab7110008e60ab3755a1c6
root <azizamine61@yahoo.fr>
2021-11-06 18:13:25 +0000

    My secodn git commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args){
-    	 System.out.println("My first java program");
+    	 System.out.println("My second ommit on linux");
     }
 }
